72398f30e7de1d600c8f8f7645ff64ed41514d1c
BongJaeChoi <bjchoi@lunasoft.co.kr>
2021-12-24 17:48:20 +0900

    타입검사공부,체크 빡시게했다. isNaN , Number.isNaN 공부까지 빡시게했네

--- src/Modal.vue
+++ src/Modal.vue
@@ -8,8 +8,10 @@
       />
       <h4>{{ onerooms[누른거].title }}</h4>
       <p>{{ onerooms[누른거].content }}</p>
-      <input v-model.number="month" />
-      <p>{{ month }}개월 선택함 : {{ onerooms[누른거].price * month }}원</p>
+      <input @input="month = $event.target.value" :value="month" />
+      <p>
+        {{ month }}개월 선택함 : {{ onerooms[누른거].price * Number(month) }}원
+      </p>
       <button @click="sendCloseModalEvent">닫기</button>
     </div>
   </div>
@@ -25,15 +27,46 @@ export default {
   },
   data() {
     return {
-      month: 1,
+      month: '1',
 
       //부모도 쓰는 데이터라면 부모 컴포넌트에 만들기....
       //데이터를 위로 전달하는건 어렵다. 아래로 전달하는건 쉽다.
       //위로는 이벤트 전달하고 아래로는 데이터를 받자.
     }
   },
+  watch: {
+    month(value) {
+      this.month = value.trim()
+
+      console.log(`${typeof value}`)
+      if (value.indexOf(' ') >= 0) {
+        alert('숫자만 입력 가능합니다.')
+        this.month = this.month.trim()
+        return
+      }
+
+      if (isNaN(value)) {
+        alert('숫자만 입력 가능합니다.')
+        this.month = '1'
+        return
+      }
+
+      //사용자가 month 에 입력한 데이터가 13보다 크면 경고문 띄우기
+      if (value > 12) {
+        alert('13이상이면 안됨!')
+        this.month = '1'
+        return
+      }
+
+      //data 영역에 있는 변수와 같은 이름을 가진 함수를 만들면 watch 발동!
+    },
+  },
   components: {},
   methods: {
+    isNumber(n) {
+      return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+    },
+
     sendCloseModalEvent() {
       this.$emit('closeModal')
     },
--- test.js
+++ test.js
@@ -1,22 +1,40 @@
-function Test(a, b){
-    this.a = a;
-    this.b = b;
+function Test(a, b) {
+  this.a = a
+  this.b = b
 }
 
 class Test2 {
-    constructor(a,b){
-        this.a = a;
-        this.b = b;
-    }
+  constructor(a, b) {
+    this.a = a
+    this.b = b
+  }
 }
 
-function add( a, b){
-   return a + b;
+function add(a, b) {
+  return a + b
 }
-  
-  
+
 //   const testInstance = new Test(1, 2)
 //   console.log(testInstance.a)
 
-const test = Test2(1,2);
-console.log(test)
+// const test = Test2(1,2);
+// console.log(test)
+
+// console.log(typeof '11ㅇㅇ' === 'string')
+const a = 1
+const b = '1'
+const c = 'a'
+const d = '1a'
+const e = ' 1'
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(`\n\n\n\n\n ${isNaN('1a')}`)
+console.log(`${e.indexOf(' ') > 0}`)
+/** result
+ a : number / parseInt = 1/ false
+ b : string / parseInt = 1 / false
+ c : string / parseInt = NaN / false
+ d : string / parseInt = 1 / false
+*/
